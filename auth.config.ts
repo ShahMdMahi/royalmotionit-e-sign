ee19ec2dd353d9a655820715d42d3c95c41dd255
Shah Md. Mahi <shahmdmahi24@gmail.com>
2025-04-16 19:51:00 +0600

    Refactor Google authentication configuration to use environment variables and add role field to User model

--- auth.config.ts
+++ auth.config.ts
@@ -8,8 +8,8 @@ import bcryptjs from "bcryptjs";
 export default {
   providers: [
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
     Credentials({
       async authorize(credentials) {
