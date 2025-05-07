3d1a9b5e34f0f1bd5c99cf70c646cb33ddb2be32
Shah Md. Mahi <shahmdmahi24@gmail.com>
2025-05-08 00:28:55 +0600

    refactor: enhance PDF viewer toolbar configuration for improved usability

--- components/common/pdf-viewer.tsx
+++ components/common/pdf-viewer.tsx
@@ -12,18 +12,43 @@ interface PDFViewerProps {
 }
 
 export function PDFViewer({ pdfData }: PDFViewerProps) {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  // Configure the default layout plugin to hide theme mode switcher and open file buttons
+  const defaultLayoutPluginInstance = defaultLayoutPlugin({
+    sidebarTabs: (defaultTabs) => defaultTabs,
+    renderToolbar: (Toolbar) => (
+      <Toolbar>
+        {(slots) => {
+          const { CurrentPageInput, Download, EnterFullScreen, GoToNextPage, GoToPreviousPage, NumberOfPages, Print, Zoom, ZoomIn, ZoomOut } = slots;
 
-  // The Viewer component can accept a Uint8Array directly for fileUrl.
+          return (
+            <div className="rpv-toolbar">
+              <div className="rpv-toolbar__left">
+                <GoToPreviousPage />
+                <CurrentPageInput />
+                <NumberOfPages />
+                <GoToNextPage />
+              </div>
+              <div className="rpv-toolbar__center">
+                <ZoomOut />
+                <Zoom />
+                <ZoomIn />
+              </div>
+              <div className="rpv-toolbar__right">
+                <EnterFullScreen />
+                <Print />
+                <Download />
+              </div>
+            </div>
+          );
+        }}
+      </Toolbar>
+    ),
+  });
 
   return (
     <Worker workerUrl="/pdf.worker.min.js">
       <div className="h-full w-full">
-        <Viewer
-          fileUrl={pdfData}
-          plugins={[defaultLayoutPluginInstance]}
-          defaultScale={SpecialZoomLevel.PageFit}
-        />
+        <Viewer fileUrl={pdfData} plugins={[defaultLayoutPluginInstance]} defaultScale={SpecialZoomLevel.PageFit} />
       </div>
     </Worker>
   );
