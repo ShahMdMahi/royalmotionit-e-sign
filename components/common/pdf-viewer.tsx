5c9c0f56b8313048c57a2ea1257a3628c72dc943
Shah Md. Mahi <shahmdmahi24@gmail.com>
2025-05-08 00:05:13 +0600

    refactor: adjust aspect ratio in PDF viewer for improved display

--- components/common/pdf-viewer.tsx
+++ components/common/pdf-viewer.tsx
@@ -8,7 +8,7 @@ import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 interface PDFViewerProps {
-  pdfData: Uint8Array; // Expecting a Uint8Array
+  pdfData: Uint8Array;
 }
 
 export function PDFViewer({ pdfData }: PDFViewerProps) {
@@ -17,16 +17,12 @@ export function PDFViewer({ pdfData }: PDFViewerProps) {
   // The Viewer component can accept a Uint8Array directly for fileUrl.
 
   return (
-    // The Worker component is essential for pdf.js to work.
-    // It points to the worker file that should be available in your public directory.
     <Worker workerUrl="/pdf.worker.min.js">
-      {" "}
-      {/* Corrected extension to .js */}
       <div className="h-full w-full">
         <Viewer
-          fileUrl={pdfData} // Pass the Uint8Array directly
+          fileUrl={pdfData}
           plugins={[defaultLayoutPluginInstance]}
-          defaultScale={SpecialZoomLevel.PageFit} // Default zoom level
+          defaultScale={SpecialZoomLevel.PageFit}
         />
       </div>
     </Worker>
